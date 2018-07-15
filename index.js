92fad37ea13c242be1b955d1841af5592769d916
orenmagid <autoimpedicis@gmail.com>
2018-07-15 13:29:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file
+function  findMatching(array, name) {
+  return array.filter(function (driver_name) {return driver_name.toLowerCase() === name.toLowerCase() });
+}
+
+function  fuzzyMatch(array, letters) {
+  const length = letters.length
+  return array.filter(function (driver_name) {return driver_name.slice(0, length) === letters });
+}
+
+function  matchName(array, string) {
+  return array.filter(function (driver) {return driver.name.toLowerCase() === string.toLowerCase() });
+}
